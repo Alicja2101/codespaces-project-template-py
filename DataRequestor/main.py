568d2220bcb5bfd292e4f07e2e092fdab03e2770
Alicja2101 <147598846+Alicja2101@users.noreply.github.com>
2024-01-12 22:39:03 +0000

    12.01

--- DataRequestor/main.py
+++ DataRequestor/main.py
@@ -1,19 +1,22 @@
-#from snap7.server import mainloop
+
 import requests
 import time
 import os
+import json
+
+
+#from dotenv import load_dotenv
+#load_dotenv()
 
 res = requests.get("https://api.openaq.org/v2/locations/2178", headers={"X-API-Key": "65a2730b52806bec0706fd72c639ceeca86934331a2309edcb8c4de84c66acd1"})
 
 #https://docs.openaq.org/docs/getting-started
 
 
-#WRO
-#url = "https://api.openaq.org/v2/locations?limit=100&page=1&offset=0&sort=desc&radius=1000&country=PL&city=Wroc%C5%82aw&order_by=lastUpdated&dump_raw=false" 
-#SOPOT
-url = "https://api.openaq.org/v2/locations?limit=100&page=1&offset=0&sort=desc&radius=1000&city=Sopot&order_by=lastUpdated&dump_raw=false"
 
-url2 = "https://api.openaq.org/v2/locations"
+url = "https://api.openaq.org/v2/locations?limit=100&page=1&offset=0&sort=desc&radius=1000&country=PL&city=Wroc%C5%82aw&order_by=lastUpdated&dump_raw=false" 
+#url = "https://api.openaq.org/v2/locations?limit=100&page=1&offset=0&sort=desc&radius=1000&city=Sopot&order_by=lastUpdated&dump_raw=false"
+
 
 headers = {"accept": "application/json"}
 
@@ -26,18 +29,17 @@ response = requests.get(url, headers=headers).json()
 
 
 ##########################zeby konsola wyplula ladny kod
-import json
+
 def jprint(obj):
     # create a formatted string of the Python JSON object
     text = json.dumps(obj, sort_keys=True, indent=5)
     print(text)
 ##########################
-
 #jprint(response.json())
 
 
 class WeatherRequester:
-    def __init__(self, city):
+    def __init__(self, city): #metody, city - artybuty
         self.city = city
         
 
@@ -57,13 +59,17 @@ class WeatherRequester:
 
 
 
-API = WeatherRequester('Wroclaw')
-#os.getenv('city')
+#API = WeatherRequester(os.getenv('MIASTO')) #ZA CHINY NIE DZIAŁĄ
+API = WeatherRequester('Wrocław')
+
+#print (os.environ)
 while (True):
+   
  
-    
+   
    #jprint(API.get_data())
     print(API.get_data())
+    
     time.sleep(10)
   
 
